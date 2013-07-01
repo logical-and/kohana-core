ce5764660a221a211c67fcadbfea358b2f38d461
And <logical_and@lavabit.com>
2013-07-01 17:34:51 +0300

    Added bootstrap.php

---
+++ bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Required by a lot of scripts (<?php defined('SYSPATH') OR die('No direct script access.');)
+if (! defined('SYSPATH')) define('SYSPATH', __DIR__ . DIRECTORY_SEPARATOR);
