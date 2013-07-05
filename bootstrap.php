4447d4b6596fb8e5b25170d2d12173c815cc8afa
And <logical_and@lavabit.com>
2013-07-05 18:43:02 +0300

    Bootstrap update: added ability to use config, user_agent, etc. methods

--- bootstrap.php
+++ bootstrap.php
@@ -3,4 +3,15 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 // Required by a lot of scripts (<?php defined('SYSPATH') OR die('No direct script access.');)
-if (! defined('SYSPATH')) define('SYSPATH', __DIR__ . DIRECTORY_SEPARATOR);
+define('SYSPATH', __DIR__ . DIRECTORY_SEPARATOR);
+
+// Other constants, obviously it can be overridden
+if (! defined('APPPATH')) define('APPPATH', __DIR__ . DIRECTORY_SEPARATOR);
+if (! defined('EXT')) define('EXT', '.php');
+
+// Config reader
+Kohana::$config = new Kohana_Config;
+Kohana::$config->attach(new Kohana_Config_File);
+
+// Imitate request, to make possible use user_agent, accept_lang etc
+Request::factory();
